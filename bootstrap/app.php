96459dc4c48fb66b16f920e8dec65b27bba9c586
bernadetaindraa <220711678@students.uajy.ac.id>
2025-11-11 12:44:19 +0700

    intergasi login ke fe

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Aktifkan Sanctum untuk stateful API
-        $middleware->statefulApi();
-
-        // Gunakan middleware CORS bawaan Laravel
-        $middleware->append(HandleCors::class);
+        $middleware->api(prepend: [
+            // Tambahkan middleware ini
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- config/cors.php
+++ config/cors.php
@@ -4,8 +4,6 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
     'supports_credentials' => true,
 ];
 
